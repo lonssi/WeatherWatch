afd63ba3919988c60cf616da15a2e829851aeeff
lonssi <laurijrapo@gmail.com>
2018-06-20 00:40:52 +0900

    Fix Font Awesome icon imports to reduce bundle size

--- app/src/index.js
+++ app/src/index.js
@@ -5,11 +5,18 @@ import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 import fontawesome from '@fortawesome/fontawesome';
-import {
-	faMapMarkerAlt, faSearch, faQuestion, faEllipsisV,
-	faSun, faThermometerHalf, faTint, faClock,
-	faBullseye, faCloud, faMoon, faFlag
-} from '@fortawesome/fontawesome-free-solid';
+import faMapMarkerAlt from '@fortawesome/fontawesome-free-solid/faMapMarkerAlt';
+import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
+import faClock from '@fortawesome/fontawesome-free-solid/faClock';
+import faQuestion from '@fortawesome/fontawesome-free-solid/faQuestion';
+import faEllipsisV from '@fortawesome/fontawesome-free-solid/faEllipsisV';
+import faSun from '@fortawesome/fontawesome-free-solid/faSun';
+import faThermometerHalf from '@fortawesome/fontawesome-free-solid/faThermometerHalf';
+import faTint from '@fortawesome/fontawesome-free-solid/faTint';
+import faFlag from '@fortawesome/fontawesome-free-solid/faFlag';
+import faBullseye from '@fortawesome/fontawesome-free-solid/faBullseye';
+import faCloud from '@fortawesome/fontawesome-free-solid/faCloud';
+import faMoon from '@fortawesome/fontawesome-free-solid/faMoon';
 
 fontawesome.library.add(
 	faMapMarkerAlt, faSearch, faClock, faQuestion, faEllipsisV, faSun,
